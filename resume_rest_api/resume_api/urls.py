f37ac89740fa622c3e75531e2b752044ee83db61
Emon Li <emonli.328@gmail.com>
2023-01-14 18:21:04 -0500

    changed double quotes to single quotes for consistency

--- resume_rest_api/resume_api/urls.py
+++ resume_rest_api/resume_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/resumes', views.ResumeList.as_view(), name="resume_list"),
-    path('api/resumes/<int:pk>', views.ResumeDetail.as_view(), name='resume_detail'),
+    path('api/resumes', views.ResumeList.as_view(), name='resume_list'),
+    path('api/resumes/<int:pk>', views.ResumeDetail.as_view(), name='resume_detail')
 ]
