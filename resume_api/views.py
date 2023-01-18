a188a84b1690dc66149245733d651c366a6e73e8
Emon Li <emonli.328@gmail.com>
2023-01-18 11:08:17 -0500

    added necessary code to process file upload

--- resume_api/views.py
+++ resume_api/views.py
@@ -2,42 +2,31 @@ from django.shortcuts import render
 import boto3
 
 # Create your views here.
-from rest_framework import generics
+from rest_framework import generics, status
 from .serializers import ResumeSerializer
 from .models import Resume
+from rest_framework.parsers import  MultiPartParser, FormParser
+from rest_framework.response import Response
+
+
 
 class ResumeList(generics.ListCreateAPIView):
     queryset = Resume.objects.all().order_by('id')
     serializer_class = ResumeSerializer
+    # everything below is taken from here to get django to process the file upload
+    # https://stackoverflow.com/questions/71241619/no-file-was-submitted-error-when-trying-to-do-an-axios-post-between-react-and-dj
+    # MultiPartParser handles file uploads in addition to static form data
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post (self, request, *args, **kwargs):
+        fileSerializer = ResumeSerializer(data=request.data)
+        if fileSerializer.is_valid():
+            fileSerializer.save()
+            return Response(fileSerializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(fileSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ResumeDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Resume.objects.all().order_by('id')
     serializer_class = ResumeSerializer
 
-
-# tried to copy the code from the flask app version in the documentation: https://devcenter.heroku.com/articles/s3-upload-python
-# don't know if it works because the /api/resumes/upload URL returns a 500 error status
-class ResumeUpload:
-    def sign_s3(file):
-        S3_BUCKET = "resumeswypestorage"
-
-        file_name = request.args.get('file_name')
-        file_type = request.args.get('file_type')
-
-        s3 = boto3.client('s3')
-
-        presigned_post = s3.generate_presigned_post(
-            Bucket = S3_BUCKET,
-            Key = file_name,
-            Fields = {"acl": "public-read", "Content-Type": file_type},
-            Conditions = [
-            {"acl": "public-read"},
-            {"Content-Type": file_type}
-            ],
-            ExpiresIn = 3600
-        )
-
-        return json.dumps({
-            'data': presigned_post,
-            'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
-        })
